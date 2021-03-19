ce4a90ac8514458c5f3cd166a2ca1d33792ee038
Noura Abdelhafez <44889573+noura-na@users.noreply.github.com>
2021-03-19 10:15:38 +0200

    Create configure_plotly_browser_state.py
    To enable the use of Plotly in Colab

---
+++ configure_plotly_browser_state.py
@@ -0,0 +1,13 @@
+def configure_plotly_browser_state():
+  import IPython
+  display(IPython.core.display.HTML('''
+        <script src="/static/components/requirejs/require.js"></script>
+        <script>
+          requirejs.config({
+            paths: {
+              base: '/static/base',
+              plotly: 'https://cdn.plot.ly/plotly-latest.min.js?noext',
+            },
+          });
+        </script>
+        '''))use of Plotly in Colab
